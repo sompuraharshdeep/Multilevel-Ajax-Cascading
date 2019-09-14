e1fdc7b6f0122456a6bb953ad0c9b95ca22af720
Harshdeep Sompura <sompuraharshdeep@gmail.com>
2019-09-14 17:54:18 +0530

    Add project files.

---
+++ MultilevelCascadingDropDown/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MultilevelCascadingDropDown.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultilevelCascadingDropDown.Controllers
+{
+    public class HomeController : Controller
+    {
+        task1Entities db = new task1Entities();
+        public ActionResult Index()
+        {
+            List<CountryMaster> countries = db.CountryMasters.ToList();
+            ViewBag.countries = new SelectList(countries, "pkCountryId", "ukCountryName", "");
+            return View();
+        }
+        public JsonResult GetStateList(int pkCountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<StateMaster> StateList = db.StateMasters.Where(x => x.refCountryId == pkCountryId).ToList();
+            return Json(StateList, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetCityList(int pkStateId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<CityMaster> CityList = db.CityMasters.Where(x => x.refStateId == pkStateId).ToList();
+            return Json(CityList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MultilevelCascadingDropDown/Models/CountryStateCityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultilevelCascadingDropDown.Models
+{
+    public class CountryStateCityViewModel
+    {
+        public int pkCountryId { get; set; }
+        public int pkStateId { get; set; }
+        public int pkCityId { get; set; }
+    }
+}
